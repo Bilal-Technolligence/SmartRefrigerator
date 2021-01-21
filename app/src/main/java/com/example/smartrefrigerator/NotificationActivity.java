aab930adafb08f8723e30a2d59e78ab538961de0
Bilal-Technolligence <nicemehar24@gmail.com>
2021-01-21 08:32:57 +0500

    firebase added

--- app/src/main/java/com/example/smartrefrigerator/NotificationActivity.java
+++ app/src/main/java/com/example/smartrefrigerator/NotificationActivity.java
@@ -1,8 +1,13 @@
 package com.example.smartrefrigerator;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class NotificationActivity extends AppCompatActivity {
 
@@ -10,5 +15,29 @@ public class NotificationActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_notification);
+        BottomNavigationView bottomNavigationView = findViewById(R.id.navigationView);
+        //set Home Seleceted
+        bottomNavigationView.setSelectedItemId(R.id.nav_notification);
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId()) {
+                    case R.id.nav_home:
+                        startActivity(new Intent(getApplicationContext(), MainActivity.class));
+                        overridePendingTransition(0,0);
+                        return true;
+                    case R.id.nav_setting:
+                        startActivity(new Intent(getApplicationContext(), SettingsActivity.class));
+                        overridePendingTransition(0,0);
+                        return true;
+                    case R.id.nav_diet:
+                        startActivity(new Intent(getApplicationContext(), DietActivity.class));
+                        overridePendingTransition(0,0);
+                        return true;
+                }
+                return false;
+            }
+        });
+
     }
 }
